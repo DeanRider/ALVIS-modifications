cbbd10f438c30ae057a0c054e51b8077bf8054a4
S. Dean Rider Jr <94093691+DeanRider@users.noreply.github.com>
2022-12-06 11:45:47 -0500

    Add files via upload

---
+++ Megapaf.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#######################################################################
+# 
+# This script will scan a folder to create a list of input sam files 
+# It will then use each file in the list it created
+# to make par files for use with Alvis
+# need to copy k8 and minimap2 misc folder into location of use
+# 
+#######################################################################
+
+# set path for running sam2paf
+export PATH="$PATH:`pwd`:`pwd`/misc" 
+# scan directory structure for sam results and store as a list
+find . -name "*.sam" -type file -exec echo '{}' >> paflist.txt \;
+
+while read mapref; do
+
+echo $mapref
+paftools.js sam2paf -p $mapref > $mapref.paf
+
+done < "paflist.txt"
+
+mkdir paffiles
+find . -name "*.sam.paf" -type file -exec cp '{}' ./paffiles/ \;
+
+exit 0
